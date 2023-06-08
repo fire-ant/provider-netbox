c033db2b2c09046223b58b2f74a7fdd822fc06c2
clavery <chris.lavery@weave.works>
2023-06-08 19:32:02 +0000

    localize

--- cty.sh
+++ cty.sh
@@ -2,6 +2,6 @@ files=($(ls package/crds))
 
 for f in ${files[@]}; do
     echo "$f"
-    ./cty generate -c package/crds/$f -o examples/generated/
+    cty generate -c package/crds/$f -o examples/generated/
 done
 
