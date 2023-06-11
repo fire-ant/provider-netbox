cc3bbf80d5319f2bde21298934005c3f26534291
clavery <chris.lavery@weave.works>
2023-06-11 15:16:07 +0100

    moved dude to import cycle issue

---
+++ config/primaryip/primaryip/config.go
@@ -0,0 +1,22 @@
+package primaryip
+
+import "github.com/upbound/upjet/pkg/config"
+
+//	Issue implementing this code as it breaks api structure by implication go import cycle
+//
+// Configure configures individual resources by adding custom ResourceConfigurators.
+func Configure(p *config.Provider) {
+	p.AddResourceConfigurator("netbox_primary_ip", func(r *config.Resource) {
+		r.ExternalName = config.IdentifierFromProvider
+		r.ShortGroup = "primaryip"
+		r.Kind = "PrimaryIp"
+		r.References["virtual_machine_id"] = config.Reference{
+			Type:      "github.com/fire-ant/provider-netbox/apis/virtualization/v1alpha1.Machine",
+			Extractor: "github.com/upbound/upjet/pkg/resource.ExtractResourceID()",
+		}
+		r.References["ip_address_id"] = config.Reference{
+			Type:      "github.com/fire-ant/provider-netbox/apis/ipam/v1alpha1.IPAddress",
+			Extractor: "github.com/upbound/upjet/pkg/resource.ExtractResourceID()",
+		}
+	})
+}
