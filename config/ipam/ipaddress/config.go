d35a3a327b038216b75b07650914f89af9653f67
clavery <chris.lavery@weave.works>
2023-06-11 20:42:27 +0100

    hack

--- config/ipam/ipaddress/config.go
+++ config/ipam/ipaddress/config.go
@@ -1,8 +1,42 @@
 package ipaddress
 
 import (
+	"github.com/crossplane/crossplane-runtime/pkg/fieldpath"
+	"github.com/crossplane/crossplane-runtime/pkg/reference"
+	"github.com/crossplane/crossplane-runtime/pkg/resource"
 	"github.com/upbound/upjet/pkg/config"
 )
+
+const (
+	// SelfPackagePath is the golang path for this package.
+	SelfPackagePath = "github.com/ipam/ipaddress"
+)
+
+var (
+	// PathInstanceGroupExtractor is the golang path to InstanceGroupExtractor function
+	// in this package.
+	PathObjectTypeSelector = SelfPackagePath + ".ObjectTypeSelector()"
+)
+
+func ObjectTypeSelector() reference.ExtractValueFn {
+	return func(mg resource.Managed) string {
+		paved, err := fieldpath.PaveObject(mg)
+		if err != nil {
+			return ""
+		}
+		var ot string
+		r, err := paved.GetString("spec.forProvider.objectType")
+		if err != nil {
+			switch r {
+			case "dcim.interface":
+				ot = "github.com/fire-ant/provider-netbox/apis/dcim/v1alpha1.DeviceInterface"
+			case "virtualization.vminterface":
+				ot = "github.com/fire-ant/provider-netbox/apis/virtualization/v1alpha1.VirtInterface"
+			}
+		}
+		return ot
+	}
+}
 
 // Configure configures individual resources by adding custom ResourceConfigurators.
 func Configure(p *config.Provider) {
@@ -11,7 +45,7 @@ func Configure(p *config.Provider) {
 		r.ShortGroup = "ipam"
 		r.Kind = "IPAddress"
 		r.References["interface_id"] = config.Reference{
-			Type:      "github.com/fire-ant/provider-netbox/apis/virtualization/v1alpha1.VirtInterface",
+			Type:      "github.com/fire-ant/provider-netbox/apis/dcim/v1alpha1.DeviceInterface",
 			Extractor: "github.com/upbound/upjet/pkg/resource.ExtractResourceID()",
 		}
 		r.References["tenant_id"] = config.Reference{
@@ -22,5 +56,6 @@ func Configure(p *config.Provider) {
 			Type:      "Vrf",
 			Extractor: "github.com/upbound/upjet/pkg/resource.ExtractResourceID()",
 		}
+		config.MarkAsRequired(r.TerraformResource, "object_type")
 	})
 }
